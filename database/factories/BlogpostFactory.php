cf7f59accb7c3d2f2621d6c666115064fac3e3e1
Donovan Druiventak <donovan.druiventak@atabix.com>
2019-08-05 16:22:08 +0200

    First commit

---
+++ database/factories/BlogpostFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Blogpost;
+use Faker\Generator as Faker;
+
+$factory->define(Blogpost::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ resources/views/mail/user-registrate.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+You have Registrated for blog post
+
+The body of your message.
+
+@component('mail::button', ['url' => ''])
+Button Text
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
